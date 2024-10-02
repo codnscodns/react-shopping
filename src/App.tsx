8952ec4fb1aaa79c6e9fd3f6320eea18e7febe75
codnscodns <dnstm1119@naver.com>
2024-10-02 15:56:50 +0900

    add register, login

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
 // src/App.tsx
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import MainContent from "./components/MainContent";
-import "./App.css";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { UserProvider } from './components/UserContext';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import MainContent from './components/MainContent';
+import Register from './components/Register';
+import Login from './components/Login';
+import './App.css';
 // 기타 필요한 컴포넌트 임포트
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Header />
-      {/* 라우트 설정 */}
-      <Routes>
-        <Route path="/" element={<MainContent />} />
-        {/* 다른 페이지 라우트 설정 */}
-      </Routes>
-      <Footer />
-    </Router>
+    <UserProvider>
+      <Router>
+        <Header />
+        {/* 라우트 설정 */}
+        <Routes>
+          <Route path='/' element={<MainContent />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </UserProvider>
   );
 };
 
